644094547786361424e7830a792c1fc4cf4ce94b
John Bushnell <jbushnell@kochava.com>
2021-06-29 14:54:57 -0700

    4.8.0

--- libKochavaAdNetworkiOS/KochavaAdNetwork.h
+++ libKochavaAdNetworkiOS/KochavaAdNetwork.h
@@ -13,8 +13,10 @@
 
 
 
+#pragma mark KochavaCore
 #import "KochavaCore.h"
 
+#pragma mark KochavaAdNetwork
 #import "KVAAdNetworkProduct.h"
 #import "KVAAdNetwork.h"
 #import "KVAAdNetworkConversion.h"
